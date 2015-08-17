33d9688226417439c191aef1f80cfd9dceb26dd0
Ribulous Barnulous <ribulous.barnulous@gmail.com>
2015-08-17 05:53:36 -0400

    fixed export

--- include/echo/iterator.h
+++ include/echo/iterator.h
@@ -3,6 +3,6 @@
 #include <echo/iterator/strided_iterator.h>
 
 namespace echo {
-using echo::iterator::strided_iterator;
+using echo::iterator::StridedIterator;
 using echo::iterator::make_strided_iterator;
 }
